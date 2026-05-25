3b54931b9d57147d70a4c73cdd7bc89dcc5639ae
iuryruansc <iuryruansc@gmail.com>
2026-05-25 15:18:46 -0300

    Entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,4 +36,4 @@ echo "Rodando migrations..."
 alembic upgrade head
 
 echo "Iniciando aplicação..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
