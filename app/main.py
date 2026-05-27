1f578c5de67cf6abd7ccbedba1b090d07a357973
iuryruansc <iuryruansc@gmail.com>
2026-05-26 22:31:37 -0300

    Add Vercel url

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        # "https://seu-app.vercel.app",  # URL do front após deploy
+        "https://service-order-web-six.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
